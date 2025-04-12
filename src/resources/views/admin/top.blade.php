69a97890dca0aca97b213d559da74b16b01ed2b4
anna <annajf1228@gmail.com>
2025-04-12 12:40:32 +0900

    管理側のTOPデザイン修正

--- src/resources/views/admin/top.blade.php
+++ src/resources/views/admin/top.blade.php
@@ -1,42 +1,25 @@
 @extends('admin.base')
 
 @section('content')
-<div class="container w-75">
-    <div class="row">
-        <div class="col mt-4 my-5">
-            <h1 class="user-top-title fs-2">管理者管理</h1>
+<div class="w-75 admin-top">
+    <div class="admin-top__content">
+        <h1 class="title">管理者管理</h1>
+        <div class="link-list">
+            <a href="{{ route('admin.index') }}">管理者管理 一覧</a>
+            <a href="{{ route('admin.create') }}">管理者管理 新規登録</a>
         </div>
     </div>
-    <div class="row">
-        <div class="col ms-5">
-            <a href="{{ route('admin.index') }}" class="user-top-a">管理者管理 一覧</a>
-        </div>
-        <div class="col">
-            <a href="{{ route('admin.create') }}" class="user-top-a">管理者管理 新規登録</a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col mt-4 my-5">
-            <h1 class="user-top-title fs-2">スタッフ管理</h1>
+    <div class="admin-top__content">
+        <h1 class="title">スタッフ管理</h1>
+        <div class="link-list">
+            <a href="{{ route('admin.user.index') }}">スタッフ管理 一覧</a>
+            <a href="{{ route('admin.user.create') }}">スタッフ管理 新規登録</a>
         </div>
     </div>
-    <div class="row">
-        <div class="col ms-5">
-            <a href="{{ route('admin.user.index') }}" class="user-top-a">スタッフ管理 一覧</a>
-        </div>
-        <div class="col">
-            <a href="{{ route('admin.user.create') }}" class="user-top-a">スタッフ管理 新規登録</a>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col mt-5 my-5">
-            <h1 class="user-top-title fs-2">スタッフ勤怠管理</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col ms-5">
-            <a href="" class="user-top-a">スタッフ勤怠管理 一覧</a>
+    <div class="admin-top__content">
+        <h1 class="title">スタッフ勤怠管理</h1>
+        <div class="link-list">
+            <a href="{{ route('admin.user-work.view') }}">スタッフ勤怠管理 一覧</a>
         </div>
     </div>
 </div>
